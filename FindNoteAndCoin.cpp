b884e582d36b01a99208068205f9be75f801f151
ShainAlee <ishinealee@gmail.com>
2019-04-15 12:18:46 +0500

    Update FindNoteAndCoin.cpp

--- FindNoteAndCoin.cpp
+++ FindNoteAndCoin.cpp
@@ -25,16 +25,16 @@ void findNoteandCoins(int s)
 	money[7] = 5;
 	money[8] = 2;
 	money[9] = 1;
-	int balance = s;//there is actually no use of creating this variable...You can just execute with s variable
+	int balance = s;//there is actually no use of creating this variable...You can just execute with s replaceds as balance
 
-	int type[10] = {0};//initailizing all array elements with 0
+	int type[10] = {0};//initailizing all array type elements with 0
 
 	for (int x = 0; x <= 9; ++x)//loop to go through the money[] elements
 	{
 			while (balance-money[x] >= 0)//if substraction of balance and money is greater then 0 the loop will execute
 			{
 				balance = balance - money[x];//substratcting money from balance
-				type[x] += 1; //incrementing type by x element
+				type[x] += 1; //incrementing type in x element
 			}
 	}
 	for (int out = 0; out <= 9; ++out)//fancy output loop
